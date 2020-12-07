318d9852f695f242fefa01de2e122586eb97f6db
unknown <jeffersonsantos.a@gmail.com>
2020-12-07 11:47:15 -0300

    Testes e TDD

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,11 +1,10 @@
-import { hash } from "bcryptjs"
-import AppError from "@shared/errors/AppError"
-
-import User from '../infra/typeorm/entities/User'
-import IUsersRepository from "../repositories/IUsersRepository"
 import { inject, injectable } from "tsyringe"
 
+import AppError from "@shared/errors/AppError"
+import IUsersRepository from "../repositories/IUsersRepository"
 
+import User from '../infra/typeorm/entities/User'
+import IHashProvider from "../providers/HashProvider/models/IHashProvider"
 
 interface IRequest {
   name: string
@@ -17,7 +16,11 @@ interface IRequest {
 class CreateUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository) { }
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
+  ) { }
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
 
@@ -27,7 +30,7 @@ class CreateUserService {
       throw new AppError('Email address already used.')
     }
 
-    const hashedPassword = await hash(password, 8)
+    const hashedPassword = await this.hashProvider.generateHash(password)
 
 
     const user = await this.usersRepository.create({
