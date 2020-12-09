6dde4e22b721570d1ca2731ee6842f0929f9a3ee
Jefferson <jeffersonsantos.a@gmail.com>
2020-12-09 15:01:05 -0300

    Testes de senha

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -2,6 +2,7 @@ import { uuid } from 'uuidv4'
 
 import UserToken from '@modules/users/infra/typeorm/entities/UserToken'
 import IUserTokensRepository from '../IUserTokensRepository'
+import User from '@modules/users/infra/typeorm/entities/User'
 
 class FakeUserTokensRepository implements IUserTokensRepository {
   private userTokens: UserToken[] = []
@@ -16,6 +17,14 @@ class FakeUserTokensRepository implements IUserTokensRepository {
     })
 
     this.userTokens.push(userToken)
+
+    return userToken
+  }
+
+  public async findByToken(token: string): Promise<UserToken> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token
+    )
 
     return userToken
   }
