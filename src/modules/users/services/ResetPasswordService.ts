1599fb4e7058e19a692e60bb8d1e36f49f7a891f
unknown <jeffersonsantos.a@gmail.com>
2020-12-10 11:20:49 -0300

    Testes Token expired

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,11 +1,10 @@
 import { inject, injectable } from "tsyringe"
+import { isAfter, addHours } from "date-fns"
 
 import AppError from "@shared/errors/AppError"
 import IUsersRepository from "../repositories/IUsersRepository"
-
-import User from '../infra/typeorm/entities/User'
-import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider"
 import IUserTokensRepository from "../repositories/IUserTokensRepository"
+import IHashProvider from "../providers/HashProvider/models/IHashProvider"
 
 interface IRequest {
   token: string
@@ -20,6 +19,9 @@ class ResetPasswordService {
 
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) { }
 
   public async execute({ token, password }: IRequest): Promise<void> {
@@ -35,7 +37,14 @@ class ResetPasswordService {
       throw new AppError('User does not exists')
     }
 
-    user.password = password
+    const tokenCreatedAt = userToken.created_at
+    const compareDate = addHours(tokenCreatedAt, 2)
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired')
+    }
+
+    user.password = await this.hashProvider.generateHash(password)
 
     await this.usersRepository.save(user)
   }
