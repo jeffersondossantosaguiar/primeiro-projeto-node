e751d6eb86422029f063ffa979b8940a55ea24cd
unknown <jeffersonsantos.a@gmail.com>
2020-12-25 17:55:26 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses'
+
+  defaults: {
+    from: {
+      email: string
+      name: string
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+    }
+  }
+} as IMailConfig
